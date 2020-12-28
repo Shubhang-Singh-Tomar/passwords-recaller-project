4f991e0a0bf611deda2e8ee948842bad60a7b8b0
Shubhang Singh Tomar <tomarshubhang@gmail.com>
2020-12-28 22:57:36 +0530

    made a pseudo-protoype

--- python_demo.py
+++ python_demo.py
@@ -1,4 +1,14 @@
-print ("\tpassword(s) recaller")
-ask_message = ("do you want me to recall a password for you")
+'''
+This is a python based programme that remembers the your password(s).
+'''
+
+print ("\n\tpassword(s) recaller".upper())                              # declares the name of the programme in capital letters.
+
+ask_message = ("do you want me to recall a password for you")           # asking message for user
 ask_message += ("\nor learn a new one ?")
-user_order = input (ask_message) 
+user_order = input (ask_message.title())                                # displays the ask_message
+
+if (user_order.lower() = "recall" or "recall a password") then :
+    print ("recalling password protocol ...")
+elif (user_order.lower() = "learn" or "learn a new password") then :
+    print ("learn new password protocol ...")
