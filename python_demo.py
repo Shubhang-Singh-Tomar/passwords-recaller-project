1e98e2256df7ced898450b6d287e6f08f4139b4b
Shubhang Singh Tomar <tomarshubhang@gmail.com>
2020-12-28 22:46:03 +0530

    decided to make a passwords recalling progamme

--- python_demo.py
+++ python_demo.py
@@ -1,1 +1,4 @@
-print ("Hello Python Again !")
+print ("\tpassword(s) recaller")
+ask_message = ("do you want me to recall a password for you")
+ask_message += ("\nor learn a new one ?")
+user_order = input (ask_message) 
