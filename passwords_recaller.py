91e47f382ff469c993c5500a51e76d56691ae0f5
Shubhang Singh Tomar <tomarshubhang@gmail.com>
2020-12-31 11:53:27 +0530

    fixed almost all bugs and limitations

--- passwords_recaller.py
+++ passwords_recaller.py
@@ -1,39 +1,52 @@
-print ("\n\tpassword(s) recaller".upper())
-def start():
-    passwords_dict = {}
+'''
+this is a python based programme that help you keep track 
+of your passcodes for several accounts
+To do :
+- need to store key-value pair permanently
+'''
+print ("\n\tpassword(s) recaller".upper())                                              # displays PASSWORD(S)_RECALLER.
+passwords_dict = {}                                                                     # declares an empty dictionary (passwords_dict).
+def start():                                                                            # declares the beginning of start() function.
+    message_one = ("\nwould you like to recall a password")                             # message for asking user to either add or recall
+    message_one += ("\nor add a new one to the dictionary ? ")                          # a password.
+    user_input_one = input (message_one + "\n[recall / add] - ")                        # stores user`s choice in a variable
 
-    message_one = ("\nWould you like to recall a password")
-    message_one += ("\nor add a new one to the dictionary ? ")
-    user_input_one = input (message_one + "\n[recall / add] - ")
-    
-    if (user_input_one == "recall") :
+    if (user_input_one == "recall") :                                                   # prints the passwords_dict if the conditions match.
         print ("\n\trecalling password protocol".upper())
+        print ("----"*8)
         print (passwords_dict)
-        rerun = input("\nwould you like to rerun the programme ? [y/n] ")
+        print ("----"*8)
+
+        rerun = input("\n=would you like to rerun the programme ? [y/n] ")               # asks user for a rerun.
         if (rerun.lower() == "y") :
             start()
         elif (rerun.lower() == "n"):
             pass
 
-    elif (user_input_one == "add") :
+    elif (user_input_one == "add") :                                                    # adds key-value pair to the password_dict .
         print ("\n\tadd new password protocol".upper())
+        def add():
+            key = input ("\n-enter username \n(with service name in brackets) : ")
+            value = input ("-enter password : ")
+            passwords_dict.update({key:value})
+            print ("key-value pair updated succesfully as :\n")
+            print (passwords_dict)
 
-        key = input ("enter username \n(with service name in brackets) : ")
-        value = input ("enter password : ")
-        passwords_dict.update({key:value})
-        print ("key-value pair updated succesfully as :\n")
-        print (passwords_dict)
+            message_two = ("\n=would you like to rerun the programme")
+            message_two += ("\nor add an another key-value pair [y/n/add] ")
+            rerun = input(message_two)                                                  # asks user for a rerun.
+            if (rerun.lower() == "y") :
+                start()
+            elif (rerun.lower() == "add") :
+                add()
+            elif (rerun.lower() == "n"):
+                pass
+        add()                                                                           # call add() function
 
-        rerun = input("\nwould you like to rerun the programme ? [y/n]")
-        if (rerun.lower() == "y") :
-            start()
-        elif (rerun.lower() == "n"):
-            pass
-
-    else :
-        print ("\n\tsomething went wrong".upper())
+    else :                                                                              # prints SOMETHING WENT WRONG 
+        print ("\n\tsomething went wrong".upper())                                      # in case of unexpected inputs.
         
-        rerun = input("would you like to rerun the programme ? [y/n]")
+        rerun = input("=would you like to rerun the programme ? [y/n]")                  # asks user for a rerun.
         if (rerun.lower() == "y") :
             start()
         elif (rerun.lower() == "n"):
